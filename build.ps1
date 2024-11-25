f3b9199ae3ddc749da40520db7c9fa9ab438f4fd
TheRealDelep <sylvain.delepee@gmail.com>
2024-11-25 17:06:08 +0100

    Make it build on windows

--- build.ps1
+++ build.ps1
@@ -1,54 +1,54 @@
 $program = "game"
-$platform = "win"
+$platform = "win32"
 $run = $false
 
-for ($i = 0; $i -lt $args.Count; i++)  {
-    $arg = $args[$i]
-    switch ($arg) {
-        "run" { $run = $true; break }
-        "pd" { $platform = "pd"; break }
+for ($i = 0; $i -lt $args.Count; $i++) {
+    switch ($args[$i]) {
+        "pd" { $platform = "playdate"; break }
         "editor" { $program = "editor"; break }
+        "run" { $run = $true; break; }
     }
 }
 
-if ($program -eq "editor" -and $platform -eq "pd") {
-    echo Cannot build editor for platform playdate
+if ($program -eq "editor" -and $platform -eq "playdate") {
+    echo "cannot build editor for playdate platform"
     return
 }
 
+$src = "platform/${platform}/main.c platform/${platform}/src/*.c game/src/*.c"
+$include = "-Igame/include -Iplatform/include"
+$libs = ""
+$link_flags = ""
+
+$out = ""
 switch ($platform) {
-    "win" { ./platform/win32/build.ps1; break }
-    "pd"  { ./platform/playdate/build.ps1; break }
-}
+    "win32" { 
+        $out = "out/game.exe" 
+        $include = "${include} -Iplatform/win32/libs/SDL/include"
 
-switch ($program) {
-    "game" { 
-        echo "Building sdl"
-        cmake -S "platform/win32/libs/SDL" -B "platform/win32/libs/SDL/build" -G "Unix Makefiles"
-        if (!$?) { return }
-        
-        cmake --build "platform/win32/libs/SDL/build"
-        if (!$?) { return }
+        if (-not (Test-Path -Path "platform/win32/libs/SDL/build/SDL3.dll")) {
+            cmake -S platform/win32/libs/SDL -B platform/win32/libs/SDL/build -G "Unix Makefiles"
+            cmake --build platform/win32/libs/SDL/build
+        }
 
-        echo "Building platform layer for win32"
-        cmake -S $PSScriptRoot -B "platform/win32/build" -G "Unix Makefiles"
-        if (!$?) { return }
-        
-        cmake --build "platform/win32/build"
-        break 
+        if (-not(Test-Path -Path "out/SDL3.dll")) {
+            cp "platform/win32/libs/SDL/build/SDL3.dll" "out/SDL3.dll"
+        }
+
+        $libs = "-L platform/win32/libs/SDL/build"
+        $link_flags = "-lSDL3"
+        break
     }
-    "editor" { ./editor/build.ps1; break }
-}
-
-if (!$run) {
-    return
-}
-
-if ($program -eq "editor") {
-    ./editor/build/editor.exe
-} else {
-    switch ($platform) {
-        "win" { ./platform/win32/build/game.exe; break }
-        "pd" { PlaydateSimulator.exe ./platform/playdate/build/game.dll; break }
+    "playdate" { 
+        $out = "out/game.dll" 
+        break
     }
 }
+
+$cc_command = "${env:cc} $src $include $libs -o $out $link_flags"
+echo $cc_command
+$res = Invoke-Expression $cc_command
+echo $res
+
+if (!$?) { return }
+& $out
--- platform/win32/main.c
+++ platform/win32/main.c
@@ -1,6 +1,33 @@
 #include "stdio.h"
+#include "game.h"
+#include "SDL3/SDL.h"
 
 int main() {
-    printf("Hello from win32!\n");
+    printf("Hello from win32! main.c\n");
+    game_hello();
+
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
+        printf("failed to initialize sdl");
+        return 1;
+    }
+
+    SDL_Window *window = SDL_CreateWindow("sdl-template", 800, 600, 0);
+    
+    if (window == NULL) {
+        printf("failed to create window");
+        return 1;
+    }
+
+    bool should_quit = false;
+    SDL_Event e;
+
+    while(!should_quit) {
+        while(SDL_PollEvent(&e)) {
+            if (e.type == SDL_EVENT_QUIT || (e.type == SDL_EVENT_KEY_DOWN && e.key.key == SDLK_ESCAPE)) {
+                should_quit = true;
+            }
+        }
+    }
+
     return 0;
 }
